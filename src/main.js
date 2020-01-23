787f638d39a691ec118fb8da47835f8d92856719
marta1014 <lovemadenghui@126.com>
2020-01-23 10:37:50 +0800

    静态资源引入 安装axios element——ui 文件夹整理

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import './styles/base.less'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+Vue.prototype.$axios = axios
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
