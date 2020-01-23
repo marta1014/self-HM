15c15145c896b05e4a786ce9b4ed3b4dbed42180
marta1014 <lovemadenghui@126.com>
2020-01-23 11:18:23 +0800

    配置基础路由-登陆

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,13 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+  { path: '/',
+    component: () => import('../views/login')
+  },
+  { path: '/login',
+    name: 'login',
+    component: () => import('../views/login')
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login"> 登陆</div>
+</template>
+
+<script>
+export default {
+  name: 'loginPage'
+}
+</script>
+
+<style>
+
+</style>
