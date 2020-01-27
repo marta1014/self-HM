38450c1311634b1e2daf50dc64929e51aaebf4df
marta1014 <lovemadenghui@126.com>
2020-01-27 20:38:16 +0800

    素材样式展示 全部/收藏 tab切换 利用activename封装业务 巧妙实现公用方法数据

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,11 +1,27 @@
 <template>
-<el-card>
+<el-card class="material">
     <breadCrumb slot="header">
     <template slot="title">素材管理</template>
     </breadCrumb>
-    <el-tabs v-model="activeName">
-        <el-tab-pane label="全部" name="all"></el-tab-pane>
-        <el-tab-pane label="收藏" name="collect"></el-tab-pane>
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+        <el-tab-pane label="全部" name="all">
+        <div class="item">
+        <el-card v-for="(item,index) of list" :key="index">
+        <img :src="item.url" alt="">
+        <el-row class="oprate">
+            <i class="el-icon-star-on"></i>
+            <i class="el-icon-delete-solid"></i>
+        </el-row>
+    </el-card>
+    </div>
+        </el-tab-pane>
+        <el-tab-pane label="收藏" name="collect">
+        <div class="item">
+        <el-card v-for="(item,index) of list" :key="index">
+        <img :src="item.url" alt="">
+    </el-card>
+    </div>
+        </el-tab-pane>
     </el-tabs>
 </el-card>
 </template>
@@ -14,13 +30,61 @@
 export default {
   data () {
     return {
-      activeName: 'all'// tab栏默认选中
+      activeName: 'all', // tab栏默认选中
+      list: []
     }
+  },
+  methods: {
+    getMaterial () { // 获取素材
+      this.$axios({
+        url: '/user/images',
+        params: { collect: this.activeName === 'collect' }
+      }).then(res => {
+        const { data } = res.data
+        this.list = data.results
+        // console.log(data)
+      })
+    },
+    changeTab () {
+      console.log(this.activeName)
+      this.getMaterial()
+    }
+  },
+  created () {
+    // 刚开始获取数据时 collect等于false 故显示全部
+    this.getMaterial()
   }
 
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.material{
+.item{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    .el-card{
+    position: relative;
+    width: 200px;
+    height: 240px;
+    margin: 20px;
+    img{
+        width: 100%;
+        height: 100%;
+    }
+    .oprate{
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        padding: 6px 0;
+        font-size: 24px;
+        display: flex;
+        background-color: #f6f6f6;
+        justify-content: space-around;
+    }
+}
+}
+}
 </style>
