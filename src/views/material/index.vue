8406be6c203495e2c39c1955ea3800a10609dd62
marta1014 <lovemadenghui@126.com>
2020-01-27 19:39:56 +0800

    tab栏实现

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,11 +3,20 @@
     <breadCrumb slot="header">
     <template slot="title">素材管理</template>
     </breadCrumb>
+    <el-tabs v-model="activeName">
+        <el-tab-pane label="全部" name="all"></el-tab-pane>
+        <el-tab-pane label="收藏" name="collect"></el-tab-pane>
+    </el-tabs>
 </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      activeName: 'all'// tab栏默认选中
+    }
+  }
 
 }
 </script>
