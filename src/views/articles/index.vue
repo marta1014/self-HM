6093d838ca2b648e60ed2441ba99e5ecee0b5951
marta1014 <lovemadenghui@126.com>
2020-01-28 12:27:44 +0800

    页面主体结构 样式处理

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -26,6 +26,23 @@
           ></el-date-picker>
       </el-form-item>
     </el-form>
+    <el-row class="title" type="flex" align="middle">
+        <span>共找到1000条内容</span>
+    </el-row>
+    <div class="articleList">
+        <div class="left">
+            <img src="../../assets/img/avatar.jpg" alt="">
+            <div class="info">
+                <span>123</span>
+                <el-tag>1234</el-tag>
+                <span class="date">2345:1234:3</span>
+            </div>
+        </div>
+        <div class="right">
+            <span><i class="el-icon-edit"></i>修改</span>
+            <span><i class="el-icon-delete"></i>删除</span>
+        </div>
+    </div>
   </el-card>
 </template>
 
@@ -63,6 +80,46 @@ export default {
 .article {
     .el-form{
         padding:50px;
+    }
+    .title{
+        height: 40px;
+        border-bottom: 1px dashed #999;
+    }
+    .articleList{
+        display: flex;
+        justify-content: space-between;
+        padding: 20px 0;
+        border-bottom: 1px solid #f2f3f5;
+        .left{
+            display: flex;
+            img{
+                width: 100px;
+                height: 100px;
+                border-radius: 6px;
+                margin-right: 10px;
+            }
+            .info{
+                display: flex;
+                flex-direction: column;
+                height: 100px;
+                justify-content: space-between;
+                .date{
+                    color:#999;
+                    font-size: 12px;
+                }
+                .el-tag{
+                    width: 60px;
+                    text-align: center;
+                }
+            }
+        }
+        .right{
+            span{
+                font-size: 14px;
+                margin-left: 6px;
+                cursor: pointer;
+            }
+        }
     }
 }
 </style>
