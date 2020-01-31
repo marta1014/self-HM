78b622f0e95c8988690bde0b353c180594d02057
marta1014 <lovemadenghui@126.com>
2020-02-01 01:58:40 +0800

    制定规则校验/ 手动触发验证 => 请求编辑=> 保存成功

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -4,22 +4,23 @@
          <breadCrumb slot="header">
          <template slot="title">账户信息</template>
          </breadCrumb>
-         <el-form label-width="100px">
-             <el-form-item label="用户名">
+         <el-form label-width="100px" ref="myform"
+         :model="formData" :rules="rules">
+             <el-form-item label="用户名" prop="name">
                  <el-input v-model="formData.name"></el-input>
              </el-form-item>
               <el-form-item label="简介">
                  <el-input v-model="formData.intro"
                  ></el-input>
              </el-form-item>
-              <el-form-item label="邮箱">
+              <el-form-item label="邮箱" prop="email">
                  <el-input v-model="formData.email"></el-input>
              </el-form-item>
               <el-form-item label="手机号">
                  <el-input disabled v-model="formData.mobile"></el-input>
              </el-form-item>
               <el-form-item>
-                 <el-button type="primary">保存信息</el-button>
+                 <el-button type="primary" @click="saveUser">保存信息</el-button>
              </el-form-item>
          </el-form>
          <el-upload action="" :show-file-list="false" class="upload">
@@ -39,6 +40,14 @@ export default {
         photo: '',
         email: '',
         mobile: ''
+      },
+      rules:
+      {
+        name: [{ required: true, message: '用户名必填' },
+          { max: 10, message: '1～10之间字符' }],
+        email: [{ required: true, message: '邮箱必填' },
+          { pattern: /^([0-9A-Za-z\-.]+)@([0-9a-z]+\.[a-z]{2,3}(\.[a-z]{2})?)$/g,
+            message: '格式不正确' }]
       }
     }
   },
@@ -49,6 +58,20 @@ export default {
       }).then(res => {
         const { data } = res.data
         this.formData = data
+      })
+    },
+    saveUser () {
+      this.$refs.myform.validate().then(() => {
+        this.$axios({
+          url: '/user/profile',
+          method: 'patch',
+          data: this.formData
+        }).then(res => {
+          this.$message({
+            type: 'success',
+            message: '保存成功'
+          })
+        })
       })
     }
   },
