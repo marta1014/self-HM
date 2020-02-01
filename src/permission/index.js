6c6e32ef708bb489b8faf4b33f83d3680cf064ce
marta1014 <lovemadenghui@126.com>
2020-02-01 13:08:46 +0800

    安装使用进度条插件

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,8 +1,10 @@
 // 导航守卫 处理导航拦截器(用token和路由拦截器处理页面权限问题)
 import router from '../router'
-
+import progress from 'nprogress'//  进度条
+import 'nprogress/nprogress.css' // 进度条样式文件
 // 全局前置守卫 （当路由发生变化时 回调函数就会执行）
 router.beforeEach((to, from, next) => {
+  progress.start()// 开启进度条
   // 权限拦截 根据当前路径如果以/home开头
   if (to.path.startsWith('/home')) {
     // 确定检查范围
@@ -15,4 +17,7 @@ router.beforeEach((to, from, next) => {
   } else {
     next()
   }
+})
+router.afterEach(() => {
+  progress.done()// 关闭进度条
 })
